ff5789e5b581ee5055968d0099581da92b4ce3d4
Januar Asprilla <ajanuarandres@gmail.com>
2024-02-13 14:08:17 -0500

    fix(Tabs):adding configurations to the repository

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,16 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "styled-components"],
+      external: [
+        "react",
+        "styled-components",
+        "react/jsx-runtime",
+        "@inubekit/foundations",
+        "@inubekit/icon",
+        "@inubekit/select",
+        "@inubekit/stack",
+        "@inubekit/text",
+      ],
       output: {
         globals: {
           react: "React",
